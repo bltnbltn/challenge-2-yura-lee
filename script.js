403ed5410397c87a09f8bc5ae336a8810944c338
Yura lee <yuralee@Yuraui-MacBook-Air.local>
2023-01-28 01:20:32 -0500

    change

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let i = 0;
 function addListItem() {
   let list = document.getElementById('list');
   let item = document.createElement('li');
-  item.id = 'sender';
+  item.className = 'sender';
   item.appendChild(document.createTextNode('Great. Thank you'));
   list.appendChild(item);
+  var el = document.getElementById('list');
+  el.className = 'sender';
+  el.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  chat.value = '';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
